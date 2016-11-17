780a149c208f5717c0faad5d9bb3efb0ee272e2a
Adrian Wawrzak <adrian.wawrzak@ericpol.com>
2016-11-17 16:28:42 +0100

    in function

--- src/com/factorial/Main.java
+++ src/com/factorial/Main.java
@@ -4,11 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         int factorialForNumber = Integer.parseInt(args[0]);
-        int s = 1;
+        int s = factorial(factorialForNumber);
+        System.out.println(s);
+    }
 
+    public static int factorial(int factorialForNumber){
+        int s = 1;
         for(int i = 1; i <= factorialForNumber; i++){
             s *= i;
         }
-        System.out.println(s);
+        return s;
     }
 }
