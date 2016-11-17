fcf7bae0ac87dd047e937a1968d8be8fba8e6984
Adrian Wawrzak <adrian.wawrzak@ericpol.com>
2016-11-17 16:24:54 +0100

    iterative

--- src/com/factorial/Main.java
+++ src/com/factorial/Main.java
@@ -3,6 +3,12 @@ package com.factorial;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int factorialForNumber = Integer.parseInt(args[0]);
+        int s = 1;
+
+        for(int i = 1; i <= factorialForNumber; i++){
+            s *= i;
+        }
+        System.out.println(s);
     }
 }
