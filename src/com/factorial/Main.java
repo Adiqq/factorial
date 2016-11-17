df054fffb047f7159b666855d50c7aa44a37a51e
Adrian Wawrzak <adrian.wawrzak@ericpol.com>
2016-11-17 16:41:30 +0100

    functional

--- src/com/factorial/Main.java
+++ src/com/factorial/Main.java
@@ -1,11 +1,18 @@
 package com.factorial;
 
+import java.util.function.Function;
+
 public class Main {
+
+    static class Wrapper {
+        Function<Integer, Integer> f;
+    }
 
     public static void main(String[] args) {
         int factorialForNumber = Integer.parseInt(args[0]);
-        int s = Factorial.calculate(factorialForNumber);
-        System.out.println(s);
+        final Wrapper w = new Wrapper();
+        w.f = x -> (x == 0 ? 1 : x * w.f.apply(x - 1));
+        System.out.println(w.f.apply(factorialForNumber));
     }
 
 }
